85d6b85f6a95db6d657ffd3ec14878b48eb1b05c
Kris <kristhesavage@gmail.com>
2019-02-09 01:00:26 +0100

    Updated README file

--- freeradius-mode.el
+++ freeradius-mode.el
@@ -2,6 +2,9 @@
 
 ;;; Commentary:
 
+;; This major-mode provides syntax highlighting and indentation handling
+;; for editing FreeRadius configuration files and unlang policies.
+
 ;;; Code:
 
 (defvar freeradius-mode-hook nil)
